4fb9d74ce334675962d7cf9ac27e026b47d2f2c0
Ajmin Pradhan <63228592+mdajminpradhan@users.noreply.github.com>
2023-02-16 01:55:16 +0600

    few change

--- components/dashboard/ShowAllTasksByAllUsers.js
+++ components/dashboard/ShowAllTasksByAllUsers.js
@@ -252,7 +252,9 @@ const ShowAllTasksByAllUsers = () => {
                     <FlagIcon className={`h-5 w-5 mx-auto ${task?.priority?.color}`} />
                   </div>
                   <p className="col-span-1 text-sm grid place-content-center border-r border-white border-opacity-25 h-full">
-                    {`${convert(task?.totalTime).hours}hr ${convert(task?.totalTime).minutes}m ${convert(task?.totalTime).seconds}s` || 'No started'}
+                    {`${!!task?.totalTime && convert(task?.totalTime).hours}hr ${!!task?.totalTime && convert(task?.totalTime).minutes}m ${
+                      !!task?.totalTime && convert(task?.totalTime).seconds
+                    }s` || 'No started'}
                   </p>
                   <p className="col-span-2 py-3 border-r border-white border-opacity-25 text-center text-sm h-full">
                     {!!task?.taskDate ? task?.taskDate : 'Unscheduled task'}
