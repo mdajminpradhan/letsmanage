6e668e0380dcc8a0e0ce2d6068a6dde308840880
Ajmin Pradhan <63228592+mdajminpradhan@users.noreply.github.com>
2023-02-16 19:24:37 +0600

    fixes

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { Menu, Transition } from '@headlessui/react';
